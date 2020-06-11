6a1d23e591d7ea5fbb5e1fcbbab9bc00caef17dd
hushixian <ishushx@gmail.com>
2020-06-11 21:12:44 +0800

    登录模块

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -4,12 +4,95 @@
       <div class="avatar_box">
         <img src="../assets/logo.png" alt="" />
       </div>
+      <!-- 登录表单 -->
+      <el-form
+        label-width="0px"
+        class="login_form"
+        :model="loginForm"
+        :rules="loginFormRules"
+        ref="loginFormRef"
+      >
+        <!-- 用户名 -->
+        <el-form-item prop="username">
+          <el-input
+            prefix-icon="el-icon-user"
+            v-model="loginForm.username"
+            clearable
+          ></el-input>
+        </el-form-item>
+        <!-- 密码 -->
+        <el-form-item prop="password">
+          <el-input
+            prefix-icon="el-icon-lock"
+            v-model="loginForm.password"
+            type="password"
+            show-password
+            clearable
+          ></el-input>
+        </el-form-item>
+        <!-- 按钮区域 -->
+        <el-form-item class="btns">
+          <el-button type="primary" @click="login">登录</el-button>
+          <el-button type="info" @click="resetLoginForm">重置</el-button>
+        </el-form-item>
+      </el-form>
     </div>
   </div>
 </template>
 
 <script>
-  export default {}
+  export default {
+    data() {
+      return {
+        //登录表单数据对象
+        loginForm: {
+          username: 'admin',
+          password: '123456',
+        },
+        //表单验证规则
+        loginFormRules: {
+          username: [
+            { required: true, message: '请输入用户名', trigger: 'blur' },
+            {
+              min: 3,
+              max: 10,
+              message: '长度在 3 到 5 个字符',
+              trigger: 'blur',
+            },
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'change' },
+            {
+              min: 6,
+              max: 15,
+              message: '长度在 6 到 15 个字符',
+              trigger: 'blur',
+            },
+          ],
+        },
+      }
+    },
+    methods: {
+      //点击重置按钮,重置表单
+      resetLoginForm() {
+        this.$refs.loginFormRef.resetFields()
+      },
+      login() {
+        this.$refs.loginFormRef.validate(async (valid) => {
+          if (!valid) {
+            return
+          }
+          const { data: res } = await this.$http.post('login', this.loginForm)
+          if (res.meta.status !== 200) {
+            return this.$message.error(res.meta.msg)
+          }
+          this.$message.success('登录成功')
+          window.sessionStorage.setItem('token', res.data.token);
+          this.$router.push('/home')
+        })
+      },
+    },
+  }
 </script>
 
 <style lang="less" scoped>
@@ -45,5 +128,17 @@
         background-color: #eeeeee;
       }
     }
+  }
+
+  .btns {
+    display: flex;
+    justify-content: flex-end;
+  }
+  .login_form {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    box-sizing: border-box;
+    padding: 0 20px;
   }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
+import axios from 'axios'
 
 Vue.config.productionTip = false
+
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+
+Vue.prototype.$http=axios
 
 new Vue({
   router,
